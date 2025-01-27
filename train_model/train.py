b80d2f5884edae4093499c2833be333e2eba8b79
Lacerta Rex Quadringentorum Viginti <102820441+LizardKing420@users.noreply.github.com>
2025-01-28 00:50:57 +0100

    Update and rename train_model/yolo/main.py to train_model/train.py

--- train_model/train.py
+++ train_model/train.py
@@ -5,8 +5,8 @@ model = YOLO("yolov8n.yaml")
 
 # Train the model
 results = model.train(
-    data="/home/RemisAI/PythonCode/yolo/config.yaml",  # path to dataset YAML
-    epochs=10,  # number of training epochs
+    data="/home/RemisAI/repos/train_model/yolo/config.yaml",  # path to dataset YAML
+    epochs=300,  # number of training epochs
     task="detect",
     # imgsz=640,  # training image size
     # device="cpu",  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
