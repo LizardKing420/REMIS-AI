10f23364b601f4deb417f30e7153e6c0e54be6b5
Ante Martic <ante.martic@protonmail.ch>
2025-01-27 18:52:18 +0100

    check_data further expanded

--- datasets/data_check.sh
+++ datasets/data_check.sh
@@ -22,34 +22,43 @@ if [ "$1" == "-r" ]; then
     REMOVE_FILES=true
 fi
 
-# Iterate through all .jpg files in the images directory
-for IMAGE_PATH in "$IMAGES_DIR"/*.jpg; do
-    # Get the base name of the image file (without extension)
-    IMAGE_NAME=$(basename "$IMAGE_PATH" .jpg)
+# Function to process subdirectories
+process_directory() {
+    local IMAGE_SUBDIR="$1"
+    local LABEL_SUBDIR="$2"
 
-    # Check if a corresponding .txt file exists in the labels directory
-    if [ ! -f "$LABELS_DIR/$IMAGE_NAME.txt" ]; then
-        DATA_FLAG=false #data is not OK
-        echo"Warning: File $IMAGE_PATH has no label!"
-        # Log the absolute path of the unmatched image
-        echo "$(realpath "$IMAGE_PATH")" >> "$LOG_FILE"
+    # Iterate through all .jpg files in the image subdirectory
+    for IMAGE_PATH in "$IMAGE_SUBDIR"/*; do
+        echo "Processing $IMAGE_PATH ..."
+        # Get the base name of the image file (without extension)
+        IMAGE_NAME=$(basename "$IMAGE_PATH" .jpg)
 
-        # Remove the image if the -r flag is set
-        if [ "$REMOVE_FILES" = true ]; then
-            rm "$IMAGE_PATH"
+        # Check if a corresponding .txt file exists in the label subdirectory
+        if [ ! -f "$LABEL_SUBDIR/$IMAGE_NAME.txt" ]; then
+            # Log the absolute path of the unmatched image
+            echo "$(realpath "$IMAGE_PATH")" >> "$LOG_FILE"
+
+            # Remove the image if the -r flag is set
+            if [ "$REMOVE_FILES" = true ]; then
+                rm "$IMAGE_PATH"
+            fi
         fi
-    fi
-done
+    done
+}
+
+# Process the test and val subdirectories
+process_directory "$IMAGES_DIR/train" "$LABELS_DIR/train"
+process_directory "$IMAGES_DIR/val" "$LABELS_DIR/val"
 
 if [ "$DATA_FLAG" = false ]; then
-    echo "Data test failed!"
+    echo -e "\033[1;31mData test failed! \033[0m"
     echo "Run the follwing line to see the files which failed the test:"
-    echo "\e[1;32m] cat $LOG_FILE \e[0m]" #Display the command in green 
+    echo -e "\033[0;31m cat $LOG_FILE \033[0m" #Display the command in green 
 
 else
-    echo "Data test passed!"  
+    echo -e "\033[1;32mData test passed!\033[0m"  
 fi
 
 # Inform the user about the result
-echo "Process completed. Check $LOG_FILE for unmatched images."
+echo "Process completed. Log-file saved at: $LOG_FILE ."
 
